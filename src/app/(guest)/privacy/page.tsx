5daf589a9af72f8201dd4802b99c2c6495944cea
Phil Wamba <32631662+philwamba@users.noreply.github.com>
2025-10-08 13:56:12 +0300

    update landing page

---
+++ src/app/(guest)/privacy/page.tsx
@@ -0,0 +1,13 @@
+import ComingSoon from '@/components/coming-soon'
+// import CTA from '@/components/cta'
+
+export default function Page() {
+    return (
+        <div className="bg-white">
+            <ComingSoon />
+
+            {/* CTA */}
+            {/* <CTA /> */}
+        </div>
+    )
+}
